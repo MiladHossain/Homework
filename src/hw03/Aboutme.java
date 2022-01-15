4c615ad9f09f359f7833282cda85f12cb438329d
MiladHossain <mhmmdmldhssn@gmail.com>
2022-01-15 14:49:28 -0500

    changed

--- src/hw03/Aboutme.java
+++ src/hw03/Aboutme.java
@@ -6,7 +6,7 @@ public String name = "Mohammad Milad hossain";
 public Byte remainingDays;
       public byte age =29;
 	
-	
+	//this is the main method
 	public static void main(String[] args) {
 		
 		
